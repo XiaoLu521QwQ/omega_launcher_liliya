4b9e960712a5bed3c905ce7d3307f8d430f74a31
Liliya233 <abc572024594@gmail.com>
2022-10-23 10:47:23 +0800

    删掉影响启动速度的压缩，以及增加Android构建

--- launcher/build.bat
+++ launcher/build.bat
@@ -1,10 +1,14 @@
+SET GOOS=windows
+go build -ldflags="-w -s" -o ../build/launcher.exe
 SET GOOS=linux
 go build -ldflags="-w -s" -o  ../build/launcher_linux
 SET GOOS=darwin
 go build -ldflags="-w -s" -o ../build/launcher_darwin
-SET GOOS=windows
-go build -ldflags="-w -s" -o ../build/launcher.exe
+SET GOOS=android
+SET GOARCH=arm64
+go build -ldflags="-w -s" -o ../build/launcher_android
 
-upx.exe -9 ../build/launcher_linux
-upx.exe -9 ../build/launcher_darwin
-upx.exe -9 ../build/launcher.exe
+:: upx.exe -9 ../build/launcher.exe
+:: upx.exe -9 ../build/launcher_linux
+:: upx.exe -9 ../build/launcher_darwin
+:: upx.exe -9 ../build/launcher_android
