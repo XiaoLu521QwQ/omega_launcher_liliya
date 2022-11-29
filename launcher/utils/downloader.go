3f981276f8294ceecc55c3d91d3c3bb4e5692b89
Liliya233 <abc572024594@gmail.com>
2022-11-29 14:07:50 +0800

    域名解析出错时使用自定义DNS地址

--- launcher/utils/downloader.go
+++ launcher/utils/downloader.go
@@ -2,9 +2,12 @@ package utils
 
 import (
 	"bytes"
+	"context"
 	"io"
+	"net"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/cheggaaa/pb/v3"
@@ -15,11 +18,25 @@ func DownloadSmallContent(sourceUrl string) []byte {
 	// Get the data
 	resp, err := http.Get(sourceUrl)
 	if err != nil {
-		pterm.Error.Println("从指定仓库下载资源时出现错误，请重试或更换仓库")
-		panic(err)
+		if strings.Contains(err.Error(), "->[::1]:53") {
+			pterm.Warning.Println("域名解析出错，建议为终端配置DNS")
+			net.DefaultResolver.Dial = func(ctx context.Context, network, address string) (net.Conn, error) {
+				dialer := &net.Dialer{
+					Timeout: 10 * time.Second,
+				}
+				return dialer.DialContext(ctx, "tcp", "8.8.8.8:53")
+			}
+			resp, err = http.Get(sourceUrl)
+			if err != nil {
+				pterm.Error.Println("从指定仓库下载资源时出现错误，请重试或更换仓库")
+				panic(err)
+			}
+		} else {
+			pterm.Error.Println("从指定仓库下载资源时出现错误，请重试或更换仓库")
+			panic(err)
+		}
 	}
 	defer resp.Body.Close()
-
 	// Size
 	size, _ := strconv.Atoi(resp.Header.Get("Content-Length"))
 	downloadSize := int64(size)
