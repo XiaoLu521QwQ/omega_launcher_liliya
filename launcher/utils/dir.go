04103b581a795ef003a4ba7ddd360bfe34f1cdbd
Liliya233 <abc572024594@gmail.com>
2022-12-16 20:35:55 +0800

    将Android目录定位至Download下

--- launcher/utils/dir.go
+++ launcher/utils/dir.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"omega_launcher/embed_binary"
 	"os"
 	"path"
 	"path/filepath"
@@ -10,6 +11,11 @@ func GetCurrentDir() string {
 	// 兼容配套的Docker
 	if IsFile(path.Join("/ome", "launcher_liliya")) {
 		return path.Join("/workspace")
+	}
+	// 兼容Android
+	plantform := embed_binary.GetPlantform()
+	if plantform == embed_binary.Android_arm64 || plantform == embed_binary.Android_x86_64 {
+		return path.Join("/sdcard", "Download")
 	}
 	pathExecutable, err := os.Executable()
 	if err != nil {
